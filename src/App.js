036cdf21182cc4676598d26f5abc8a383a38aa82
Carlos Barragan <starttheshooting@hotmail.com>
2017-12-05 15:51:06 -0600

    Square Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Knight from './components/Knight/Knight';
+import Square from './components/Square/Square';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Knight/>
+        <Square black>
+          <Knight/>
+        </Square>
       </div>
     );
   }
