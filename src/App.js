ff99e0349406231989884e775f4e0a230b216040
Carlos Barragan <starttheshooting@hotmail.com>
2017-12-06 08:40:58 -0600

    Board Flex positions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board/Board';
 
@@ -7,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Board knigthPosition={[0,0]}/>
+        <Board knightPosition={[7,0]}/>
       </div>
     );
   }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,19 +4,41 @@ import Square from '../Square/Square';
 import Knight from '../Knight/Knight';
 
 export default class Board extends Component {
-    render(){
-        return (
-            <div>
-                <Square black>
-                    <Knight/>
+
+    renderSquare(squarePosition) {
+
+        const x = squarePosition % 8;
+        const y = Math.floor(squarePosition / 8);
+        const isBlack = (x + y) % 2 === 1;
+        const [knightX, knightY] =  this.props.knightPosition;
+        const hasPiece = (x === knightX && y === knightY) ? <Knight/> : null;
+    
+        return(
+            <div key={squarePosition} style={{width: '12.5%', height: '12.5%'}}>
+                <Square black={isBlack}>
+                    {hasPiece}
                 </Square>
+            </div>
+        );
+    }
+
+    render(){
+        const squares = [];
+
+        for (let i = 0; i < 64; i++){
+            squares.push(this.renderSquare(i));
+        }
+
+        return (
+            <div style={{ width: '100%', height: '100%', display: 'flex', flexWrap: 'wrap'}}>
+                {squares}
             </div>
         );
     }
 }
 
 Board.propTypes = {
-    knigthPosition: PropTypes.arrayOf(
+    knightPosition: PropTypes.arrayOf(
         PropTypes.number.isRequired
     ).isRequired
 };
